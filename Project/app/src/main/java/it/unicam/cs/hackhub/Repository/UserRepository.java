d963bc476533f50b31466e598ac13e9ba63ee748
Giulio Parrucci <giuparrucc@gmail.com>
2026-01-22 16:22:10 +0100

    Added mock repositories

---
+++ Project/app/src/main/java/it/unicam/cs/hackhub/Repository/UserRepository.java
@@ -0,0 +1,28 @@
+package it.unicam.cs.hackhub.Repository;
+
+import it.unicam.cs.hackhub.Model.Entity.Hackathon;
+import it.unicam.cs.hackhub.Model.Entity.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRepository {
+
+    private static final Map<Long, User> repo = new HashMap<>();
+    private static Long serialId = 1L;
+
+    public User get(Long id) {
+        return repo.get(id);
+    }
+
+    public User put(User u) {
+        if (u.getId() == null) u.setId(serialId++);
+        repo.put(u.getId(), u);
+        return u;
+    }
+
+    public void remove(Long id) {
+        repo.remove(id);
+    }
+
+}
