c9620d62286e8a8900f95a5e463559ce85ccb897
marzetti-matteo <marzetti.matteo@gmail.com>
2026-01-23 15:40:12 +0100

    added NotificationService method signature

--- Project/app/src/main/java/it/unicam/cs/hackhub/Repository/NotificationRepository.java
+++ Project/app/src/main/java/it/unicam/cs/hackhub/Repository/NotificationRepository.java
@@ -3,6 +3,7 @@ package it.unicam.cs.hackhub.Repository;
 import it.unicam.cs.hackhub.Model.Entity.Notification;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class NotificationRepository {
@@ -12,6 +13,10 @@ public class NotificationRepository {
 
     public Notification get(Long id) {
         return repo.get(id);
+    }
+
+    public List<Notification> getAll() {
+        return repo.values().stream().toList();
     }
 
     public void put(Notification u) {
