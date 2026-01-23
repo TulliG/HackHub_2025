5b7890d85afa8b2a0926271d689b9ecd751cdb12
marzetti-matteo <marzetti.matteo@gmail.com>
2026-01-23 15:41:12 +0100

    added TeamService method signature

--- Project/app/src/main/java/it/unicam/cs/hackhub/Service/TeamService.java
+++ Project/app/src/main/java/it/unicam/cs/hackhub/Service/TeamService.java
@@ -1,13 +1,26 @@
 package it.unicam.cs.hackhub.Service;
 
-
 import it.unicam.cs.hackhub.Repository.TeamRepository;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
  * Service class for managing {@code Team}s
  */
 public class TeamService {
 
-    TeamRepository repo = new TeamRepository();
+    private final TeamRepository repo = new TeamRepository();
 
+    private final UserService userService;
+
+    public TeamService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void createNewTeam() {
+
+    }
+
+    public void quitTeam(@NonNull Long userId) {
+
+    }
 }
