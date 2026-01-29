e7c20776b0a9b3b98cd7aaff9efa2d9403aba2ef
marzetti-matteo <marzetti.matteo@gmail.com>
2026-01-29 11:12:27 +0100

    added HackathonController methods signature and TODOs

--- Project/app/src/main/java/it/unicam/cs/hackhub/Controllers/HackathonController.java
+++ Project/app/src/main/java/it/unicam/cs/hackhub/Controllers/HackathonController.java
@@ -5,7 +5,55 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HackathonController {
 
+    public void createHackathon() {
+        //TODO implement: create new Hackathon from builder
+    }
+
+    public void getHackathonsInSubscription() {
+        //TODO implement: return all Hackathon with Hackthon.getState() == State.SUBSCRIPTION
+    }
+
     public void getAll() {
         //TODO implement: return all Hackathon + ConcludedHackathons
+    }
+
+    public void registerTeam() {
+        //TODO implement: add a team in Hackathon
+    }
+
+    public void uploadSubmission() {
+        //TODO implement: add a submission in Hackathon
+    }
+
+    public void cancelRegistration() {
+        //TODO implement: remove Team from Hackathon
+    }
+
+    public void proclaimWinner() {
+        //TODO implement: select Hackathon's winner
+    }
+
+    public void getMentors() {
+        //TODO implement: return all Hackathon's mentors
+    }
+
+    public void rateSubmission() {
+        //TODO implement: set Submission' s grade
+    }
+
+    public void getAppointments() {
+        //TODO implement: return all Hackathon's Appointments
+    }
+
+    public void getHackathonTeams() {
+        //TODO implement: return all Hackathon's Teams
+    }
+
+    public void getSubmissions() {
+        //TODO implement: return all Hackathon's submissions
+    }
+
+    public void sendRequest() {
+        //TODO implement: send support request in Hackathon
     }
 }
