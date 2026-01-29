b2cfb88ff168aac3d4d9f929c86a796b8d360ab4
marzetti-matteo <marzetti.matteo@gmail.com>
2026-01-29 10:45:37 +0100

    added NotificationController method signatures and TODOs

--- Project/app/src/main/java/it/unicam/cs/hackhub/Controllers/NotificationController.java
+++ Project/app/src/main/java/it/unicam/cs/hackhub/Controllers/NotificationController.java
@@ -1,4 +1,21 @@
 package it.unicam.cs.hackhub.Controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NotificationController {
+
+    public void accept() {
+        //TODO implement: call facade accept method with involved notification
+    }
+
+    public void getAllInvites() {
+        //TODO implement: return all notication with notification.getType().isInvite
+    }
+
+    public void showDetails() {
+        //TODO implement: show notification's details
+    }
+
+
 }
