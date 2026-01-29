d291b3418a91577e6e2bc8cde0d9696baffcfc98
marzetti-matteo <marzetti.matteo@gmail.com>
2026-01-29 10:45:28 +0100

    added HackathonController method signatures and TODOs

--- Project/app/src/main/java/it/unicam/cs/hackhub/Controllers/HackathonController.java
+++ Project/app/src/main/java/it/unicam/cs/hackhub/Controllers/HackathonController.java
@@ -1,4 +1,11 @@
 package it.unicam.cs.hackhub.Controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HackathonController {
+
+    public void getAll() {
+        //TODO implement: return all Hackathon + ConcludedHackathons
+    }
 }
