ca39170898ed01344beaf86777eeb994949bd981
marzetti-matteo <marzetti.matteo@gmail.com>
2026-01-29 10:45:49 +0100

    added TeamController method signatures and TODOs

--- Project/app/src/main/java/it/unicam/cs/hackhub/Controllers/TeamController.java
+++ Project/app/src/main/java/it/unicam/cs/hackhub/Controllers/TeamController.java
@@ -1,4 +1,11 @@
 package it.unicam.cs.hackhub.Controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TeamController {
+
+    public void createTeam() {
+        //TODO implement: create team and add user creator as member
+    }
 }
